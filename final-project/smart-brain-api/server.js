14685e7611ab58777f3f89f75bc90bdc56b81b0d
Omar Osuna <omosuna98@gmail.com>
2019-08-04 20:12:24 -0700

    Final project finished, production and deployment left

--- final-project/smart-brain-api/server.js
+++ final-project/smart-brain-api/server.js
@@ -16,40 +16,7 @@ const db = knex({
   }
 });
 
-// db.select('*').from('users') returns a promise
-db.select('*')
-  .from('users')
-  .then(data => {
-    console.log(data);
-  });
-
 const app = express();
-
-// Fake data before we learn how to use databases
-
-// If we didn't have a database, we would need to loop through this whole array
-// and imagine if the array gets bigger and bigger, the time for searching for an
-// specific value like password would take too much.
-const database = {
-  users: [
-    {
-      id: '123',
-      name: 'John',
-      email: 'john@gmail.com',
-      password: 'cookies',
-      entries: 0,
-      joined: new Date()
-    },
-    {
-      id: '124',
-      name: 'Sally',
-      email: 'sally@gmail.com',
-      password: 'bananas',
-      entries: 0,
-      joined: new Date()
-    }
-  ]
-};
 
 app.use(bodyParser.json());
 
@@ -60,85 +27,101 @@ app.get('/', (req, resp) => {
 });
 
 app.post('/signin', (req, res) => {
-  /*bcryptjs.compare(
-    'apples',
-    '$2a$08$.EO8ZHWHYdG79LKvOFK.yez7P/tbWG0dG/g8h/YbLzhGLSZSJDleS',
-    function(err, res) {
-      console.log('first guess', res);
-    }
-  );
-  bcryptjs.compare(
-    'veggies',
-    '$2a$08$.EO8ZHWHYdG79LKvOFK.yez7P/tbWG0dG/g8h/YbLzhGLSZSJDleS',
-    function(err, res) {
-      console.log('second guess', res);
-    }
-  );*/
-  if (
-    req.body.email === database.users[0].email &&
-    req.body.password == database.users[0].password
-  ) {
-    res.json(database.users[0]);
-  } else {
-    res.status(400).json('error logging in');
-  }
+  const { email, password } = req.body;
+
+  db.select('email', 'hash')
+    .from('login')
+    .where('email', '=', email)
+    .then(data => {
+      const isValid = bcryptjs.compareSync(password, data[0].hash);
+
+      if (isValid) {
+        return db
+          .select('*')
+          .from('users')
+          .where('email', '=', email)
+          .then(user => {
+            res.json(user[0]);
+          })
+          .catch(err => res.status(400).json('unable to get user'));
+      } else {
+        res.status(400).json('wrong credentials');
+      }
+    })
+    .catch(err => res.status(400).json('wrong credentials'));
 });
 
 // Always remember to respond in order to not hang out waiting the user when accessing
 // to our server endpoint
 app.post('/register', (req, res) => {
   const { name, email, password } = req.body;
-  db('users')
-    // returning('*'): We can do this instead of doing another select statement
-    // This says insert the user in our users table and return all the columns
-    .returning('*')
-    .insert({
-      email: email,
-      name: name,
-      joined: new Date()
-    })
-    .then(user => {
-      // We are not returning an array
-      res.json(user[0]);
-    })
-    .catch(err => res.status(400).json('unable to register'));
+
+  const hash = bcryptjs.hashSync(password);
+
+  // Making a transaction to avoid inconsistencies
+  // This transaction is probably the trickiest part to get use to it
+  db.transaction(trx => {
+    trx
+      .insert({
+        hash: hash,
+        email: email
+      })
+      .into('login')
+      .returning('email')
+      .then(loginEmail => {
+        return (
+          // returning('*'): We can do this instead of doing another select statement
+          // This says insert the user in our users table and return all the columns
+          trx('users')
+            .returning('*')
+            .insert({
+              email: loginEmail[0],
+              name: name,
+              joined: new Date()
+            })
+            .then(user => {
+              // We are not returning an array
+              res.json(user[0]);
+            })
+        );
+      })
+      .then(trx.commit)
+      .catch(trx.rollback);
+  }).catch(err => res.status(400).json('unable to register'));
 });
 
 app.get('/profile/:id', (req, res) => {
   const { id } = req.params;
   let found = false;
 
-  // The thing is that with forEach, it will return a value in it's own scope
-  // until it iterates through the whole array
-  database.users.forEach(user => {
-    if (user.id === id) {
-      found = true;
-      return res.json(user);
-    }
-  });
-
-  if (!found) {
-    res.status(400).json('Not found');
-  }
+  db.select('*')
+    .from('users')
+    // where receives and object with the value we want to filter
+    .where({ id })
+    .then(user => {
+      // If the array is not empty it means that it return a user
+      if (user.length) {
+        res.json(user[0]);
+      } else {
+        res.status(400).json('Not found');
+      }
+    })
+    .catch(err => res.status(400).json('error getting user'));
 });
 
 // Is the same thing as using POST, it would work the same if we
 // decided to use POST but we are following the standards
 app.put('/image', (req, res) => {
   const { id } = req.body;
-  let found = false;
 
-  database.users.forEach(user => {
-    if (user.id === id) {
-      found = true;
-      user.entries++;
-      return res.json(user.entries);
-    }
-  });
-
-  if (!found) {
-    res.status(400).json('Not found');
-  }
+  db('users')
+    .where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+      res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json('unable to get entries'));
 });
 
 app.listen(8080, () => {
